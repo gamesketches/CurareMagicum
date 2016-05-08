6521fd54a2c291f3e0773617f3eb9be8a5b84432
gamesketches <sam.von.ehren@gmail.com>
2016-05-08 17:01:39 -0400

    getting spell names implemented

--- Assets/Scripts/SpellInterpreter.cs
+++ Assets/Scripts/SpellInterpreter.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
 
 public class SpellInterpreter : MonoBehaviour {
 
+	string[] spellNames;
+	string[] cureRatings;
+
 	// Use this for initialization
 	void Start () {
-	
+		StreamReader sr = File.OpenText("Assets/Resources/spellNames.csv");
+		spellNames = sr.ReadToEnd().Split('\n').ToArray();
+
+		Debug.Log(spellNames[2].Split(',')[3]);
 	}
 	
 	// Update is called once per frame
