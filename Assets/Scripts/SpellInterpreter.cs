0f295c7731f0ac373a2640b275ff545badd0126f
gamesketches <sam.von.ehren@gmail.com>
2016-05-09 10:53:34 -0400

    less insane version of the spell interpreter

--- Assets/Scripts/SpellInterpreter.cs
+++ Assets/Scripts/SpellInterpreter.cs
@@ -4,15 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.IO;
 
+public enum Element {fire, water, wind, earth, metal, wood};
+
 public class SpellInterpreter : MonoBehaviour {
 
-	string firstSlot, secondSlot, thirdSlot;
+	Element firstSlot, secondSlot, thirdSlot;
 	string[] spellNames;
 	string[] cureRatings;
 	string[] symptoms;
 	string[] areaCures;
 	string[] levels;
-	string[] spells = new string[6] {"fire", "water", "wind", "earth", "metal", "wood"};
 	public int level = 0;
 	private int maxLevel;
 
@@ -22,80 +23,92 @@ public class SpellInterpreter : MonoBehaviour {
 		spellNames = sr.ReadToEnd().Split('\n').ToArray();
 		sr.Close();
 
-		sr = File.OpenText("Assets/Resources/symptomChart");
+		sr = File.OpenText("Assets/Resources/symptomChart.csv");
 		symptoms = sr.ReadToEnd().Split('\n');
 		sr.Close();
 
-		sr = File.OpenText("Assets/Resources/cureRatings");
+		sr = File.OpenText("Assets/Resources/cureRatings.csv");
 		cureRatings = sr.ReadToEnd().Split('\n');
 		sr.Close();
 
-		sr = File.OpenText("Assets/Resources/areaCures");
+		sr = File.OpenText("Assets/Resources/areaCures.csv");
 		areaCures = sr.ReadToEnd().Split('\n');
 		sr.Close();
 
-		sr = File.OpenText("Assets/Resources/levels");
+		sr = File.OpenText("Assets/Resources/levels.csv");
 		levels = sr.ReadToEnd().Split('\n');
 		sr.Close();
 
 		maxLevel = levels.Length - 1;
+
 	}
 
-	void genNewAnswer() {
-		firstSlot = spells[Random.Range(0, 6)];
-		secondSlot = spells[Random.Range(0, 6)];
-		thirdSlot = spells[Random.Range(0, 6)];
+	public void genNewAnswer() {
+		firstSlot = (Element)Random.Range(0,6);
+		secondSlot = (Element)Random.Range(0,6);
+		thirdSlot = (Element)Random.Range(0,6);
+		Debug.Log(string.Concat("New answer= ", firstSlot.ToString(), secondSlot.ToString(), thirdSlot.ToString()));
 	}
 
-	void nextLevel() {
+	public void nextLevel() {
 		if(level < maxLevel) {
 			level++;
-			firstSlot = levels[level].Split(',')[0];
-			secondSlot = levels[level].Split(',')[1];
-			thirdSlot = levels[level].Split(',')[2];
+			firstSlot = Parse(levels[level].Split(',')[0]);
+			secondSlot = Parse(levels[level].Split(',')[1]);
+			thirdSlot = Parse(levels[level].Split(',')[2]);
 		}
 	}
 
-	string getSymptoms() {
-		return string.Concat(symptoms[getSpellIndex(firstSlot)].Split(',')[0],
-							symptoms[getSpellIndex(secondSlot)].Split(',')[1],
-							symptoms[getSpellIndex(thirdSlot)].Split(',')[2]);
+	public string getSymptoms() {
+		return string.Concat(symptoms[(int)firstSlot].Split(',')[0],
+			symptoms[(int)secondSlot].Split(',')[1],
+							symptoms[(int)thirdSlot].Split(',')[2]);
 	}
 
-	string getSpellName(string firstElement, string secondElement, string thirdElement) {
-		return firstElement == secondElement && thirdElement == secondElement ? 
-			string.Concat(spellNames[getSpellIndex(firstSlot)].Split(',')[6], spellNames[getSpellIndex(firstSlot)].Split(',')[getSpellIndex(secondSlot)],
-			areaCures[getSpellIndex(thirdSlot)].Split(',')[0]) : string.Concat(spellNames[getSpellIndex(firstSlot)].Split(',')[getSpellIndex(secondSlot)],
-													areaCures[getSpellIndex(thirdSlot)].Split(',')[0]);
+	public string getSpellName(string firstElement, string secondElement, string thirdElement) {
+		Element first = Parse(firstElement);
+		Element second = Parse(secondElement);
+		Element third = Parse(thirdElement);
+		string coreSpell = string.Concat(spellNames[(int)first].Split(',')[(int)second],
+			areaCures[(int)third].Split(',')[0]);
+		if(firstElement == secondElement && thirdElement == secondElement) { 
+			coreSpell = string.Concat(spellNames[(int)first].Split(',')[6], coreSpell);
+		}
+
+		return coreSpell;
 	}
 
-	int checkAnswer(string firstElement, string secondElement, string thirdElement) {
+	public int checkAnswer(string firstElement, string secondElement, string thirdElement) {
+		Element first = Parse(firstElement);
+		Element second = Parse(secondElement);
+		Element third = Parse(thirdElement);
+
 		int score = 0;
 
-		if(firstElement == firstSlot) {
+		if(first == firstSlot) {
 			score += 2;
 		}
-		else if( symptoms[getSpellIndex(firstElement)].Split(',')[0] == symptoms[getSpellIndex(firstSlot)].Split(',')[0]) {
+		else if( symptoms[(int)first].Split(',')[0] == symptoms[(int)firstSlot].Split(',')[0]) {
 			score += 1;
 		}
 
-		if(secondElement == secondSlot) {
+		if(second == secondSlot) {
 			score += 1;
 		}
 
-		if(thirdElement == thirdSlot || areaCures[getSpellIndex(thirdSlot)].Split(',')[2] == areaCures[getSpellIndex(thirdElement)].Split(',')[1]) {
+		if(third == thirdSlot || areaCures[(int)thirdSlot].Split(',')[2] == areaCures[(int)third].Split(',')[1]) {
 			score += 4;
 		}
 		return score;
 	}
 
-	string getDiseaseSolved() {
-		return string.Concat(getSeverity(), symptoms[getSpellIndex(firstSlot)].Split(',')[0],
-			symptoms[getSpellIndex(secondSlot)].Split(',')[1], symptoms[getSpellIndex(thirdSlot)].Split(',')[2]);
+	public string getDiseaseSolved() {
+		return string.Concat(getSeverity(), symptoms[(int)firstSlot].Split(',')[0],
+			symptoms[(int)secondSlot].Split(',')[1], symptoms[(int)thirdSlot].Split(',')[2]);
 	}
 
-	string getSeverity() {
-		string[] curSpell = new string[3] {firstSlot, secondSlot, thirdSlot};
+	public string getSeverity() {
+		string[] curSpell = new string[3] {firstSlot.ToString(), secondSlot.ToString(), thirdSlot.ToString()};
 		if(levels.ToArray().Distinct().Count() == 1) {
 			return "Lethal";
 		}
@@ -115,13 +128,14 @@ public class SpellInterpreter : MonoBehaviour {
 		return "";
 	}
 
-	int getSpellIndex(string elementName) {
-		for(int i = 0; i < spells.Length; i++) {
-			if(elementName == spells[i]) {
-				return i;
+	Element Parse(string elementName) {
+		int numElements = System.Enum.GetValues(typeof(Element)).Length;
+			for(Element elementType = Element.fire; (int)elementType < numElements; elementType++){
+				if(elementType.ToString() == elementName) {
+					return elementType;
+				}
 			}
+			Debug.LogError("Invalid type sent");
+		return Element.fire;
 		}
-		Debug.LogError("illegal element " + elementName + " found");
-		return -1;
-	}
 }
